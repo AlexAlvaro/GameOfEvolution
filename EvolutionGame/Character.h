b8f4fdd7a269b84dbc3c0c6596d026c4311e70a7
Jose Pinto Villamar <pintovillamar@outlook.com>
2018-06-23 17:53:47 -0500

    ordenado

--- EvolutionGame/Character.h
+++ EvolutionGame/Character.h
@@ -3,23 +3,23 @@
 
 class Character
 {
-  protected:
-    int coins;
-    bool accion;
+protected:
+  int coins;
+  bool accion;
 
-  public:
-    Character(int C, bool A);
-    ~Character(){};
-    int get_coins();
-    void set_coins(int C);
-    int get_acc();
-    void set_acc(bool acc);
-    virtual void interactua();
-    virtual bool actuando(bool ac, int turno);
-	void Ganando();
-	void Perdiendo();
-	void EmpaGan();
-	void EmpaPer();
+public:
+  Character(int C, bool A);
+  ~Character(){};
+  int get_coins();
+  void set_coins(int C);
+  int get_acc();
+  void set_acc(bool acc);
+  virtual void interactua();
+  virtual bool actuando(bool ac, int turno);
+  void Ganando();
+  void Perdiendo();
+  void EmpaGan();
+  void EmpaPer();
 };
 
 #include "Character.inl"
