f73c78478e545849aea4950564cf6671016b3017
Jose Pinto Villamar <pintovillamar@outlook.com>
2018-06-27 11:32:32 -0500

    added spaces to make it look clean

--- EvolutionGame/CopyCat.h
+++ EvolutionGame/CopyCat.h
@@ -4,16 +4,15 @@
 
 class CopyCat : public Character
 {
-public:
-  CopyCat(int C, bool A) : Character(C, A){
+  public:
+    CopyCat(int C, bool A) : Character(C, A){
 
-  };
-  ~CopyCat()
-  {
+                             };
+    ~CopyCat(){
 
-  };
-  void interactua();
-  bool actuando(bool ac, int turno);
+    };
+    void interactua();
+    bool actuando(bool ac, int turno);
 };
 #include "Copycat.inl"
 #endif
