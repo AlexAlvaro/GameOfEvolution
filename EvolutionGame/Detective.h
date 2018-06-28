6cda77d6fe8bbe2a7f13199f84b094d9fbd57c0a
José Pinto Villamar <pintovillamar@outlook.com>
2018-06-28 09:42:36 -0500

    Añadadio el método existente get_ren() y set_ren() para Detective.

--- EvolutionGame/Detective.h
+++ EvolutionGame/Detective.h
@@ -6,14 +6,18 @@ class Detective : public Character
 {
   private:
     int anteriorAc;
+    int Rencor;
 
   public:
     Detective(int C, bool A) : Character(C, A){
       anteriorAc = 1;
+      Rencor = 0;
     };
     ~Detective(){};
 
     bool get_anterior();
+    int get_ren();
+    void set_ren(int r);
 
     void interactua();
     bool actuando(bool ac, int turno);
